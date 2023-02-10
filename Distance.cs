c14fae1a6f87a47a6ac7f7e102768519eaaeae1b
Alexandr Popov <He1ton@yandex.ru>
2023-02-10 05:13:57 +0300

    Rewrite Scripts

--- Distance.cs
+++ Distance.cs
@@ -6,31 +6,7 @@ public class Distance : MonoBehaviour
 {
     [SerializeField] private Transform _cubePosition;
     [SerializeField] private Transform _passDistance;
-    [SerializeField] private float _speed;
 
-    public bool _moving;
-
-    private Vector3 _target;
-
-    private void Start()
-    {
-        _target = _passDistance.position;
-    }
-
-    private void Update()
-    {
-        transform.LookAt(_target);
-        Move();
-    }
-
-    public void Move()
-    {
-        if (_cubePosition.position != _passDistance.position)
-        {
-            if (_moving)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
-            }
-        }
-    }
+    public Transform PassDistance => _passDistance;
+    public Transform CubePosition => _cubePosition;
 }
--- Patrolling.cs
+++ Patrolling.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public class Patrolling : MonoBehaviour
 {
-    [SerializeField] private List<Transform> _runers;
+    [SerializeField] private float _speed;
+    [SerializeField] private bool _moving;
+
+    public Distance _distance;
+
+    private Vector3 _target;
+
+    private void Start()
+    {
+        _target = _distance.PassDistance.position;
+    }
+
+    private void Update()
+    {
+        transform.LookAt(_target);
+        Move();
+    }
+
+    public void Move()
+    {
+        if (_moving)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+        }
+    }
+
 }
