8824cbd6ea1da2477c0ff1bcb81d80414d1e6210
Alexandr Popov <He1ton@yandex.ru>
2023-02-13 19:32:06 +0300

    Rewrite code

--- Patrolling.cs
+++ Patrolling.cs
@@ -4,44 +4,56 @@ using UnityEngine;
 
 public class Patrolling : MonoBehaviour
 {
-    [SerializeField] private Transform _point1;
-    [SerializeField] private Transform _point2;
+    [SerializeField] private List<Transform> _allPointList;
     [SerializeField] private float _speed;
     [SerializeField] private bool _moving;
 
     private Vector3 _target;
+    private int _index = 0;
 
     private void Start()
     {
-        _target = _point1.position;
+        _target = _allPointList[_index].position;
     }
 
     private void Update()
     {
-        transform.LookAt(_target);
         Move();
     }
 
     public void Move()
     {
-        transform.LookAt(_target);
         transform.Rotate(0, 0, 1);
+        transform.LookAt(_target);
 
         if (_moving)
         {
             transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
         }
-
+     
         if (transform.position == _target)
         {
-            if (_target == _point1.position)
+            if (_target == _allPointList[_index].position)
             {
-                _target = _point2.position;
+                _target = _allPointList[_index + 1].position;
             }
-            else if (_target == _point2.position)
+            else if (_target == _allPointList[_index + 1].position)
             {
-                _target = _point1.position;
+                _target = _allPointList[_index + 2].position;
             }
+            else if (_target == _allPointList[_index+2].position)
+            {
+                _target = _allPointList[_index+3].position;
+            }
+            else if (_target == _allPointList[_index + 3].position)
+            {
+                _target = _allPointList[_index + 4].position;
+            }
+            else if (_target == _allPointList[_index + 4].position)
+            {
+                _target = _allPointList[_index+3].position; 
+            }
+        
         }
     }
 }
