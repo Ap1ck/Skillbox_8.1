f757280ce46a3d48553a627e415eda24d303d1ec
Alexandr Popov <He1ton@yandex.ru>
2023-02-16 04:17:15 +0300

    Rewrite code

--- Patrolling.cs
+++ Patrolling.cs
@@ -21,39 +21,23 @@ public class Patrolling : MonoBehaviour
         Move();
     }
 
-    public void Move()
+    private void Move()
     {
         transform.Rotate(0, 0, 1);
         transform.LookAt(_target);
 
-        if (_moving)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
-        }
-     
+        transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+
         if (transform.position == _target)
         {
-            if (_target == _allPointList[_index].position)
+            if (_target == _allPointList[_index++].position)
             {
-                _target = _allPointList[_index + 1].position;
+                _target = _allPointList[_index].position;
             }
-            else if (_target == _allPointList[_index + 1].position)
+            else if (_target == _allPointList[_index--].position)
             {
-                _target = _allPointList[_index + 2].position;
+                _target = _allPointList[_index].position;
             }
-            else if (_target == _allPointList[_index+2].position)
-            {
-                _target = _allPointList[_index+3].position;
-            }
-            else if (_target == _allPointList[_index + 3].position)
-            {
-                _target = _allPointList[_index + 4].position;
-            }
-            else if (_target == _allPointList[_index + 4].position)
-            {
-                _target = _allPointList[_index+3].position; 
-            }
-        
         }
     }
 }
